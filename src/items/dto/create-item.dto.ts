d0486edffe8bb8a4fadf79b5f1ef6a561f13680f
J-su35 <det_a248@hotmail.com>
2024-10-21 23:25:33 +0700

    first commit and CRUD

---
+++ src/items/dto/create-item.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateItemDto {
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    amount: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    price: number;
+    
+}
---
+++ src/items/entities/item.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum ItemStatus {
+    PENDING = 'PENDING', APPROVED = 'APPROVED', REJECTED = 'REJECTED'
+}
+
+@Entity()
+export class Item {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    title: string;
+
+    @Column()
+    amount: number;
+
+    @Column()
+    price: number;
+
+    @Column({
+        nullable: false,
+        default: ItemStatus.PENDING
+    })
+    status: ItemStatus
+}
