63fc2c2ba06702fba8f9e0513cfb0cca2aa796c2
J-su35 <det_a248@hotmail.com>
2024-10-21 23:39:11 +0700

    add users module

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum Role {
+    USER = 'USER',
+    ADMIN = 'ADMIN',
+    MANAGER = 'MANAGER'
+}
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({
+        unique: true
+    })
+    username: string;
+    
+    @Column()
+    password: string;
+    
+    @Column()
+    role: Role
+}
