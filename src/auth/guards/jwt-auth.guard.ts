bab3168a1b7c17df00feb0a701d848bb30976ddf
J-su35 <det_a248@hotmail.com>
2024-10-22 20:32:04 +0700

    jwt-expire completed

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JsonWebTokenError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
+function mapMessage(info: JsonWebTokenError): string {
+  if (info.message.includes('malformed')) {
+    return `Malformed token`
+  }
+
+  if (info.message.includes('expired')) {
+    return `token expired`
+  }
+
+  return info.message
+}
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  //refresh token & validate message
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      console.log('err', err)
+      console.log('info', info)
+      throw new UnauthorizedException(mapMessage(info));
+    }
+    return user;
+  }
+
+}
