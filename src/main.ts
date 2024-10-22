4acf0a0be23fb692e2a91ea738e5fbcb3d2bb64e
J-su35 <det_a248@hotmail.com>
2024-10-22 20:01:07 +0700

    add QueryFailedError

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { QueryFailedErrorFilter } from './filters/query-failed-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  //waiting
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true }
+  //   )
+  // )
+  
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  app.useGlobalFilters(new QueryFailedErrorFilter(httpAdapter))
+
+
+  
+
+  // app.enableCors(); // waiting for CORS
+
   await app.listen(3000);
 }
 bootstrap();
